e4b6b723284908ed368ed80c1e2230ff83c850df
lovehzj <1922919664@qq.com>
2023-02-13 01:10:53 +0800

    change logname from mysql to mariadb

--- docker-build/scripts/start-mariadb.sh
+++ docker-build/scripts/start-mariadb.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 mkdir -p $MARIADB_HOME/logs
-rm -rf /etc/mysql/my.cnf.fallback
-rm -rf /etc/mysql/mysql.cnf
 rm -rf /etc/mysql/my.cnf
+rm -rf /etc/mysql/my.cnf.fallback
+rm -rf /etc/mysql/mariadb.cnf
 rm -rf /etc/mysql/conf.d
 rm -rf /etc/mysql/mysql.conf.d
 CONF_FILE=/etc/mysql/my.cnf
 echo "[mysqld]" >$CONF_FILE
 echo "bind-address = 0.0.0.0" >>$CONF_FILE
-echo "mysqlx-bind-address = 0.0.0.0" >>$CONF_FILE
-echo "key_buffer_size  = 16M" >>$CONF_FILE
-echo "myisam-recover-options  = BACKUP" >>$CONF_FILE
-echo "log_error = $MARIADB_HOME/logs/mysql_error.log" >>$CONF_FILE
+echo "character-set-server  = utf8mb4" >>$CONF_FILE
+echo "collation-server      = utf8mb4_general_ci" >>$CONF_FILE
+echo "log_error = $MARIADB_HOME/logs/mariadb_error.log" >>$CONF_FILE
 echo "max_binlog_size   = 100M" >>$CONF_FILE
 echo "ssl = 0" >>$CONF_FILE
 echo "[mysqldump]" >>$CONF_FILE
